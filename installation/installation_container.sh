9e2f847ae93708d233486699a9189e529e759f26
Jicquel Vincent <vincent.jicquel@gmail.com>
2017-10-31 11:19:00 +0100

    bapt exe cmd

--- installation/installation_container.sh
+++ installation/installation_container.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 
-if [ "$UID" -ne "0" ]; then
-	echo "Installation non privilégiée!"
-else
-	echo "Installation privilégiée!"
-fi
 #on vide le fichier de configuration par défaut.
-
 echo "" > /etc/lxc/default.conf 
 
 ./moveScripts.sh
